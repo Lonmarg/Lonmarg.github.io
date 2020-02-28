c48aa938f6853cd4f06c0d54ffc71fc34b47937c
Geoffry <hammong@oregonstate.edu>
2020-02-28 17:04:38 -0500

    back to no changes

--- routes/home.js
+++ routes/home.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const mysql = require('mysql');
 
 router.get('/', function(req, res, next) {
-	let pool = mysql.createPool({
-		connectionLimit: 10,
-		host: 'classmysql.engr.oregonstate.edu',
-		user: 'cs340_wellheup',
-		password: 'Akirr@5t@r5und3r',
-		database: 'cs340_wellheup'
-	});
+	// let pool = mysql.createPool({
+		// connectionLimit: 10,
+		// host: 'classmysql.engr.oregonstate.edu',
+		// user: 'cs340_wellheup',
+		// password: 'Akirr@5t@r5und3r',
+		// database: 'cs340_wellheup'
+	// });
    
     let context = {};
 	// pool.query("SELECT * FROM armylists", function(err, result)
