6abbe63e5c1f151e4788f6cd7956d1005925f6ea
Phil <knamustarsunder@gmail.com>
2020-02-28 11:58:32 -0800

    commented get/

--- routes/home.js
+++ routes/home.js
@@ -2,29 +2,29 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
-router.get('/', function(req, res, next) {
-	let pool = mysql.createPool({
-		connectionLimit: 10,
-		host: 'classmysql.engr.oregonstate.edu',
-		user: 'cs340_wellheup',
-		password: 'Akirr@5t@r5und3r',
-		database: 'cs340_wellheup'
-	});
+// router.get('/', function(req, res, next) {
+// 	let pool = mysql.createPool({
+// 		connectionLimit: 10,
+// 		host: 'classmysql.engr.oregonstate.edu',
+// 		user: 'cs340_wellheup',
+// 		password: 'Akirr@5t@r5und3r',
+// 		database: 'cs340_wellheup'
+// 	});
 
-	console.log("get/ is trying to query");
+// 	console.log("get/ is trying to query");
 	
-	pool.query("SELECT * FROM armylists", function(err, result)
-	{
-		if(err)
-		{
-			next(err);
-			return;
-		}
+// 	pool.query("SELECT * FROM armylists", function(err, result)
+// 	{
+// 		if(err)
+// 		{
+// 			next(err);
+// 			return;
+// 		}
 		
-		res.render('home', result);
+// 		res.render('home', result);
 
-	});
-});
+// 	});
+// });
 
 router.get('/', function(req, res, next){
     context = {};
