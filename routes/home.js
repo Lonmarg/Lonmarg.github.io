53df92f45eacd6cc5ba5f05ba6be5d167e564ea0
Phil <knamustarsunder@gmail.com>
2020-02-28 14:55:13 -0800

    updates might actually work now

--- routes/home.js
+++ routes/home.js
@@ -3,37 +3,29 @@ const router = express.Router();
 const mysql = require('mysql');
 
 router.get('/', function(req, res, next) {
-	// let pool = mysql.createPool({
-		// connectionLimit: 10,
-		// host: 'classmysql.engr.oregonstate.edu',
-		// user: 'cs340_wellheup',
-		// password: 'Akirr@5t@r5und3r',
-		// database: 'cs340_wellheup'
-	// });
+	let pool = mysql.createPool({
+		connectionLimit: 10,
+		host: 'classmysql.engr.oregonstate.edu',
+		user: 'cs340_wellheup',
+		password: 'Akirr@5t@r5und3r',
+		database: 'cs340_wellheup'
+	});
    
     let context = {};
     context.subtitle = "words";
-	// pool.query("SELECT * FROM armylists", function(err, result)
-	// {
-		// if(err)
-		// {
-			// next(err);
-			// return;
-		// }
-        // // context = JSON.stringify(result);
-        // // result.subtitle = "subtitle";
-		
-		// // var stuffToDisplay = {};
-		// // stuffToDisplay.headertext = "I passed some stuff to handlebars via home.js!";
-		
-		// // res.render('home', stuffToSend);
-        // //res.send('test text')
-	// });
-	
-	// var stuffToDisplay = {};
-	// stuffToDisplay.headertext = "I passed some stuff to handlebars via home.js!";
-		
-	res.render('home', context);
+	pool.query("SELECT * FROM armylists", function(err, result)
+	{
+		if(err)
+		{
+			next(err);
+			return;
+		}
+        
+        context.headertext = JSON.stringify(result);
+
+        res.render('home', context);
+	});
+
 });
 
 module.exports = router;
