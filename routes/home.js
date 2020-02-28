85fecdcce2a39cbf03e0affcd5bd1e5ce7532809
Phil <knamustarsunder@gmail.com>
2020-02-27 18:05:59 -0800

    query attempt

--- routes/home.js
+++ routes/home.js
@@ -2,26 +2,45 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
-router.get('/', function(req, res, next) {
-		let pool = mysql.createPool({
-		connectionLimit: 10,
-		host: 'classmysql.engr.oregonstate.edu',
-		user: 'cs340_wellheup',
-		password: 'Akirr@5t@r5und3r',
-		database: 'cs340_wellheup'
-	});
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_wellheup',
+    password: 'Akirr@5t@r5und3r',
+    database: 'cs340_wellheup'
+});
 
-	pool.query("SELECT * FROM armylists", function(err, result)
-	{
-		if(err)
-		{
-			next(err);
-			return;
-		}
+// router.get('/', function(req, res, next) {
+// 		let pool = mysql.createPool({
+// 		connectionLimit: 10,
+// 		host: 'classmysql.engr.oregonstate.edu',
+// 		user: 'cs340_wellheup',
+// 		password: 'Akirr@5t@r5und3r',
+// 		database: 'cs340_wellheup'
+// 	});
+
+// 	pool.query("SELECT * FROM armylists", function(err, result)
+// 	{
+// 		if(err) throw err;
 		
-		res.render('equipments', result);
+		
+// 		res.render('equipments', result);
 
-	});
+// 	});
+// });
+
+router.get('/', function(req, res, next) {
+    console.log("I decided to run get/");
+    var context = {};
+    pool.query('SELECT * FROM armylists', function(err, rows, fields) {
+        if (err) {
+            next(err + "/");
+            return;
+        }
+        context.results = JSON.stringify(rows);
+		context.table = rows;
+		res.render('home', context);
+    });
 });
 
 module.exports = router;
